c50881a4ad33b778599a34ffb5469c6b119f69bc
Joseph Mitchell Magen <jmagen@redhat.com>
2013-01-09 18:14:44 +0200

    updated require statement

--- lib/foreman_plugin_template.rb
+++ lib/foreman_plugin_template.rb
@@ -1,3 +1,3 @@
 module ForemanPluginTemplate
-  require 'engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+  require 'foreman_plugin_template/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
 end
